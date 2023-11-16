7a8727e056228d7629fb3c0220f91d95d8690e75
Mona Lisa <anastasiia.rn@gmail.com>
2023-11-15 21:42:21 -0500

    header

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0F6039",
+        secondary: "#FF914D"
+      }
+    },
   },
   plugins: [],
 }
