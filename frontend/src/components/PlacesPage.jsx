e3b6114c0db29202c2153802ed80632083deea0c
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-11-17 19:46:33 -0600

    Add helper function and some css

--- frontend/src/components/PlacesPage.jsx
+++ frontend/src/components/PlacesPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import Perks from "../Perks";
+import axios from "axios";
 
 const PlacesPage = () => {
   //states
@@ -33,6 +34,15 @@ const PlacesPage = () => {
       </>
     );
   };
+
+  async function addPhotoUsingLink(e) {
+    e.preventDefault();
+    const {data:filename} = await axios.post('/upload-by-link', {link: photoLink})
+    setAddedPhoto(prev => {
+      return [...prev, filename];
+    });
+    setPhotoLink('');
+  }
 
   return (
     <div>
@@ -87,12 +97,17 @@ const PlacesPage = () => {
                 onChange={(e) => setPhotoLink(e.target.value)}
                 placeholder={"add using a link .....jpg"}
               />
-              <button className="bg-gray-200 px-4 rouded-2xl">
+              <button onClick={addPhotoUsingLink} className="bg-gray-200 px-4 rouded-2xl">
                 Add&nbsp; Photo
               </button>
             </div>
-            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-              <button className="flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+              {addedPhotos.lenghth > 0 && addedPhotos.map(link => (
+                <div>
+                 <img className="rounded-2xl" src={"http://localhost:4000/uploads/"+link} alt="" />
+                </div>
+              ))}
+              <button className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
