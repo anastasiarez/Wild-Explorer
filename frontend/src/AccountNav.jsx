a6055835969bb43556689e469fde9cdb04c9ef97
Mona Lisa <anastasiia.rn@gmail.com>
2023-11-19 13:26:55 -0500

    add styles to Account page

--- frontend/src/AccountNav.jsx
+++ frontend/src/AccountNav.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const AccountNav = () => {
   const { pathname } = useLocation();
   let subpage = pathname.split("/")?.[2];
-  if(subpage === undefined){
-    subpage = 'profile';
+  if (subpage === undefined) {
+    subpage = "profile";
   }
 
   function linkClasses(type = null) {
     let classes = "inline-flex gap-1 py-2 px-6 rounded-full";
 
     if (type === subpage) {
-      classes += "bg-primary text-white ";
+      classes += "bg-primary text-white rounded-full";
     } else {
       classes += "bg-gray-200";
     }
@@ -20,20 +20,26 @@ const AccountNav = () => {
   }
 
   return (
-    <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
-      <Link className={linkClasses("profile")} to={"/account"}>
-        {" "}
-        My Profile{" "}
+    <nav className="w-full flex justify-center mt-10 gap-10 mb-10">
+      <Link
+        className={`${linkClasses("profile")} py-2 px-4 bg-primary text-white rounded-full`}
+        to={"/account"}
+      >
+        My Profile
       </Link>
 
-      <Link className={linkClasses("bookings")} to={"/account/bookings"}>
-        {" "}
+      <Link
+        className={`${linkClasses("bookings")} py-2 px-4 bg-primary text-white rounded-full`}
+        to={"/account/bookings"}
+      >
         My Bookings
       </Link>
 
-      <Link className={linkClasses("places")} to={"/account/places"}>
-        {" "}
-        My Accomodations{" "}
+      <Link
+        className={`${linkClasses("places")} py-2 px-4 bg-primary text-white rounded-full`}
+        to={"/account/places"}
+      >
+        My Places
       </Link>
     </nav>
   );
