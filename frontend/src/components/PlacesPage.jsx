c29e128d52d9eb3b3522ee1bd46a5cd2972b53f3
Mona Lisa <anastasiia.rn@gmail.com>
2023-11-20 20:12:01 -0500

    improve register & login functionality

--- frontend/src/components/PlacesPage.jsx
+++ frontend/src/components/PlacesPage.jsx
@@ -42,7 +42,7 @@ const PlacesPage = () => {
       </div>
       <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {places.length === 0 ? (
-          <p>Loading...</p>
+          <p>...You don't have listings yet</p>
         ) : (
           places.map((place) => (
             <Link
