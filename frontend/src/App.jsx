df03cd66e7b0180b91e37010c4c146b1623a189c
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-11-23 03:40:14 -0600

    Fixed conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,13 @@ import Layout from "./Layout";
 import RegisterPage from "./components/RegisterPage";
 import axios from "axios";
 import { UserContextProvider } from "./UserContext";
-import PlacesPage from "./components/PlacesPage";
+
 import ProfilePage from "./components/ProfilePage";
 import PlacesFormPage from "./components/PlacesFormPage";
-import PlacePage from "./components/PlacePage"; //Silvia
+import PlacesPage from "./components/PlacesPage";
 import Search from "./Search";
 import { useState } from "react";
-import PlacePage from "./components/PlacePage";
+
 import BookingsPage from "./components/BookingsPage";
 import BookingPage from "./components/BookingPage";
 
@@ -26,44 +26,24 @@ function App() {
   return (
     <UserContextProvider>
       <Routes>
-        <Route
-          path="/"
-          element={<Layout setSearchResults={setSearchResults} setSearchButtonClick={setSearchButtonClick} />}
-        >
-          <Route index element={<IndexPage searchResults={searchResults} searchButtonClick={searchButtonClick} />} />
+        <Route path="/" element={<Layout setSearchResults={setSearchResults} setSearchButtonClick={setSearchButtonClick}/>}>
+          <Route index element={<IndexPage searchResults={searchResults} searchButtonClick={searchButtonClick}/>}/>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
 
           <Route path="/account" element={<ProfilePage />} />
-          <Route path="/account/bookings" element={<PlacesPage />} />
+          {/* <Route path="/account/bookings" element={<PlacesPage />} /> */}
 
           <Route path="/account/places" element={<PlacesPage />} />
           <Route path="/account/places/new" element={<PlacesFormPage />} />
           <Route path="/account/places/:id" element={<PlacesFormPage />} />
-          <Route path="/place/:id" element={<PlacePage />} />
+          <Route path="/place/:id" element={<PlacesPage />} />
+          <Route path="/account/bookings" element={<BookingsPage />} />
+          <Route path="/account/bookings/:id" element={<BookingPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
   );
-    return (
-        <UserContextProvider>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<IndexPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
-                    <Route path="/account" element={<ProfilePage />} />
-                    {/* <Route path="/account/bookings" element={<PlacesPage />} /> */}
-                    <Route path="/account/places" element={<PlacesPage />} />
-                    <Route path="/account/places/new" element={<PlacesFormPage />} />
-                    <Route path="/account/places/:id" element={<PlacesFormPage />} />
-                    <Route path="/place/:id" element={<PlacePage />} />
-                    <Route path="/account/bookings" element={<BookingsPage />} />
-                    <Route path="/account/bookings/:id" element={<BookingPage />} />
-                </Route>
-            </Routes>
-        </UserContextProvider>
-    );
 }
 
 export default App;
