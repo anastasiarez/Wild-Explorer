76db28f80b126aa0968de923abf798aff421a634
Mona Lisa <anastasiia.rn@gmail.com>
2023-11-30 16:02:08 -0500

    stripe feature

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,22 @@ const Booking = require("./models/Booking.js");
 const Review = require("./models/Review.js");
 const reviewsRouter = express.Router();
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const bodyParser = require('body-parser');
+
+const path = require('path');
+const { token } = require("morgan");
+const { log } = require("console");
+const port = process.env.PORT || 5173;
+
 // Routes
 const app = express();
 app.use("/reviews", reviewsRouter);
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     credentials: true,
@@ -68,6 +78,24 @@ function getUserDataFromReq(req) {
     );
   });
 }
+////// STRIPE //////
+
+app.post('/place/:id/process-payment', async (req, res) => {
+  const { token, amount } = req.body;
+
+  try {
+    await stripe.charges.create({
+      source: token.id,
+      amount,
+      currency: "CAD"
+    });
+
+    res.json({ status: 'Success' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to process payment' });
+  }
+});
 
 
 ////////  REGISTER, LOGIN/LOGOUT & PROFILE  ///////////
@@ -202,7 +230,7 @@ app.post("/places", (req, res) => {
       });
 
 
-      res.json(placeDoc); // previous part run up to here
+      res.json(placeDoc);
     } catch (error) {
       console.error("Error creating place:", error);
       res.status(500).json({ error: "Internal Server Error" });
@@ -328,21 +356,21 @@ app.post('/bookings/:bookingId', async (req, res) => {
     // price,
   } = req.body;
 
-  Booking.updateOne({user:userData.id},
+  Booking.updateOne({ user: userData.id },
     {
-    checkIn,
-    checkOut,
-    // place,
-    // numberOfGuests,
-    // name,
-    // phone,
-    // price,
-    // user: userData.id,
-  }).then((doc) => {
-    res.json(doc);
-  }).catch((err) => {
-    throw err;
-  });
+      checkIn,
+      checkOut,
+      // place,
+      // numberOfGuests,
+      // name,
+      // phone,
+      // price,
+      // user: userData.id,
+    }).then((doc) => {
+      res.json(doc);
+    }).catch((err) => {
+      throw err;
+    });
 });
 
 app.get("/bookings", async (req, res) => {
@@ -407,77 +435,77 @@ app.get("/property/:propertyId", async (req, res) => {
 });
 
 app.patch('/:reviewId', async (req, res) => {
-    try {
-        const { reviewId } = req.params;
-        const { rating, comment } = req.body;
-        const userData = await getUserDataFromReq(req); // Your authentication logic to get user data
+  try {
+    const { reviewId } = req.params;
+    const { rating, comment } = req.body;
+    const userData = await getUserDataFromReq(req); // Your authentication logic to get user data
 
-        // Validate user data and check if they are the owner of the review
-        if (!userData) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-
-        // Optional validation checks
-        if (rating < 1 || rating > 5 || !comment.trim()) {
-            return res.status(400).json({
-                error: 'Invalid rating or comment'
-            });
-        }
-
-        // Check if the user is the owner of the review
-        const existingReview = await Review.findById(reviewId);
-        if (!existingReview) {
-            return res.status(404).json({ error: 'Review not found' });
-        }
-        if (existingReview.user.toString() !== userData.userId) {
-            return res.status(403).json({ error: 'Forbidden' });
-        }
-
-        // Update the review in the database
-        const updatedReview = await Review.findByIdAndUpdate(
-            reviewId,
-            { rating, comment },
-            { new: true } // Return the updated document
-        );
-
-        // Respond with the updated review
-        res.json(updatedReview);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+    // Validate user data and check if they are the owner of the review
+    if (!userData) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
+
+    // Optional validation checks
+    if (rating < 1 || rating > 5 || !comment.trim()) {
+      return res.status(400).json({
+        error: 'Invalid rating or comment'
+      });
+    }
+
+    // Check if the user is the owner of the review
+    const existingReview = await Review.findById(reviewId);
+    if (!existingReview) {
+      return res.status(404).json({ error: 'Review not found' });
+    }
+    if (existingReview.user.toString() !== userData.userId) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    // Update the review in the database
+    const updatedReview = await Review.findByIdAndUpdate(
+      reviewId,
+      { rating, comment },
+      { new: true } // Return the updated document
+    );
+
+    // Respond with the updated review
+    res.json(updatedReview);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Delete a review
 app.delete('/:reviewId', async (req, res) => {
-    try {
-        const { reviewId } = req.params;
-        const userData = await getUserDataFromReq(req); // Your authentication logic to get user data
+  try {
+    const { reviewId } = req.params;
+    const userData = await getUserDataFromReq(req); // Your authentication logic to get user data
 
-        if (!userData) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-      // Find the review to ensure it exists and to check ownership
-        const reviewToDelete = await Review.findById(reviewId);
-        if (!reviewToDelete) {
-            return res.status(404).json({ error: 'Review not found' });
-        }
-
-        // Check if the user is the owner of the review or an admin
-        if (reviewToDelete.user.toString() !== userData.userId) {
-            return res.status(403).json({ error: 'Forbidden' });
-        }
-
-        // Delete the review from the database
-        const deletedReview = await Review.findByIdAndDelete(reviewId);
-
-        // Respond with success message
-        res.json({ message: 'Review successfully deleted', deletedReview });
-    } catch (error) {
-        if (error.kind === 'ObjectId') {
-            return res.status(400).json({ error: 'Invalid review ID format' });
-        }
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!userData) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
+    // Find the review to ensure it exists and to check ownership
+    const reviewToDelete = await Review.findById(reviewId);
+    if (!reviewToDelete) {
+      return res.status(404).json({ error: 'Review not found' });
+    }
+
+    // Check if the user is the owner of the review or an admin
+    if (reviewToDelete.user.toString() !== userData.userId) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    // Delete the review from the database
+    const deletedReview = await Review.findByIdAndDelete(reviewId);
+
+    // Respond with success message
+    res.json({ message: 'Review successfully deleted', deletedReview });
+  } catch (error) {
+    if (error.kind === 'ObjectId') {
+      return res.status(400).json({ error: 'Invalid review ID format' });
+    }
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Add this endpoint to handle deleting a booking by ID
@@ -486,17 +514,17 @@ app.delete('/bookings/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-      // Find the booking by ID and the user ID to ensure ownership
-      const booking = await Booking.findOneAndDelete({ _id: id, user: userData.id });
+    // Find the booking by ID and the user ID to ensure ownership
+    const booking = await Booking.findOneAndDelete({ _id: id, user: userData.id });
 
-      if (!booking) {
-          return res.status(404).json({ error: 'Booking not found or unauthorized' });
-      }
+    if (!booking) {
+      return res.status(404).json({ error: 'Booking not found or unauthorized' });
+    }
 
-      res.json({ message: 'Booking deleted successfully' });
+    res.json({ message: 'Booking deleted successfully' });
   } catch (error) {
-      console.error('Error deleting booking:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error deleting booking:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
--- frontend/src/BookingWidget.jsx
+++ frontend/src/BookingWidget.jsx
@@ -1,3 +1,262 @@
+// import axios from "axios";
+// import { useContext, useEffect, useState } from "react";
+// import { differenceInCalendarDays, eachDayOfInterval, addDays } from "date-fns";
+// import { useNavigate } from "react-router-dom";
+// import { UserContext } from "./UserContext.jsx";
+// import ReactDatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
+
+// // import { loadStripe } from '@stripe/stripe-js';
+// // import { Elements } from '@stripe/react-stripe-js';
+// // import { useStripe } from '@stripe/react-stripe-js';
+// // import { stripePromise } from './StripeContext'; 
+// // import CheckoutForm from "./components/CheckoutForm.jsx";
+
+
+// export function excludeSingleDate(bookedDates) {
+//   const missingDates = [];
+
+//   for (let i = 0; i < bookedDates.length - 1; i++) {
+//     const currentDate = (bookedDates[i]);
+//     const nextDate = (bookedDates[i + 1]);
+
+//     const interval = eachDayOfInterval({ start: currentDate, end: nextDate });
+
+//     if (interval.length > 1) {
+//       const missingDatesInInterval = interval.slice(1, -1);
+//       missingDates.push(...missingDatesInInterval);
+//     }
+//   }
+
+//   return [...missingDates, ...bookedDates];
+
+// }
+
+// export function removeDuplicates(dates) {
+//   return [...new Set(dates.map(date => date.getTime()))].map(date => new Date(date)).sort((a, b) => a.getTime() - b.getTime());
+// }
+
+
+// export default function BookingWidget({ place }) {
+
+//   const { user } = useContext(UserContext);
+//   const [name, setName] = useState('');
+//   const [phone, setPhone] = useState('');
+//   const [checkIn, setCheckIn] = useState(null);
+//   const [checkOut, setCheckOut] = useState(null);
+//   const [numberOfGuests, setNumberOfGuests] = useState('1');
+//   const [bookedDates, setBookedDates] = useState([]);
+//   const [maxGuests, setMaxGuests] = useState(1);
+//   const [bookingId, setBookingId] = useState(null);
+//   const [bookingSuccess, setBookingSuccess] = useState(false);
+//   const [errorMessage, setErrorMessage] = useState("");
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     axios.get(`/bookings/${place._id}`).then(response => {
+//       const result = [];
+//       for (const { checkIn, checkOut } of response.data) {
+//         const bookingPeriod = eachDayOfInterval({
+//           start: new Date(checkIn),
+//           end: new Date(checkOut)
+//         });
+//         result.push(...bookingPeriod);
+
+//       }
+//       setBookedDates(excludeSingleDate(removeDuplicates(result)));
+
+//     });
+//   }, []);
+
+//   useEffect(() => {
+//     if (user) {
+//       setName(user.name);
+//     }
+//   }, [user]);
+
+//   useEffect(() => {
+//     setMaxGuests(place.maxGuests);
+//   }, [place]);
+
+//   let numberOfNights = 0;
+
+//   if (checkIn && checkOut) {
+//     numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
+//   }
+
+//   async function bookThisPlace() {
+
+//     if (!user) {
+
+//       alert('Please log in to book this place');
+//       //navigate('/login');
+//       return;
+//     }
+
+//     if (parseInt(numberOfGuests, 10) > maxGuests) {
+//       return;
+//     }
+
+//     try {
+//       if (numberOfNights < 2) {
+//         setErrorMessage("Minimum booking duration is two nights.");
+//         return;
+//       }
+
+//       const response = await axios.post('/bookings', {
+//         checkIn,
+//         checkOut,
+//         numberOfGuests,
+//         name,
+//         phone,
+//         place: place._id,
+//         price: numberOfNights * place.price,
+//       });
+
+//       const newBookingId = response.data._id;
+
+//       setBookingSuccess(true);
+//       setBookingId(newBookingId);
+
+//       setTimeout(() => {
+//         navigate(`/account/bookings/`);
+//       }, 1000);
+
+
+//     }
+//     catch (error) {
+//       console.error("Failed to book:", error);
+//       setErrorMessage("Please register or loging");
+//     }
+//   }
+
+//   const isPhoneNumberValid = (phoneNumber) => /^\d{10}$/.test(phoneNumber);
+
+//   const isFormValid = () => {
+//     return (
+//       checkIn !== null &&
+//       checkOut !== null &&
+//       numberOfGuests !== '' &&
+//       name !== '' &&
+//       isPhoneNumberValid(phone)
+//     );
+//   };
+
+//   return (
+//     <div className="bg-white shadow p-4 rounded-2xl">
+//       <div className="text-2xl text-center">
+//         Price: ${place.price} / per night
+//       </div>
+
+//       <div className="border rounded-2xl mt-8">
+//         <div className="flex">
+//           <div className="py-5 px-5 border-l">
+
+//             <label>Check-In: </label>
+//             <ReactDatePicker
+//               selected={checkIn}
+//               onChange={date => {
+//                 setCheckIn(date);
+//                 setErrorMessage(null);
+//               }}
+//               minDate={addDays(new Date, 1)}
+//               excludeDates={bookedDates}
+//               dateFormat="yyyy/MM/dd"
+//             />
+//           </div>
+//           <div className="py-5 px-5 border-l">
+
+//             <label>Check-Out: </label>
+//             <ReactDatePicker
+//               selected={checkOut}
+//               onChange={date => {
+//                 setCheckOut(date);
+//                 setErrorMessage(null);
+//               }}
+//               minDate={checkIn || addDays(new Date, 1)}
+//               excludeDates={bookedDates}
+//               dateFormat="yyyy/MM/dd"
+//             />
+//           </div>
+//         </div>
+//         <div className="py-3 px-4 border-t">
+
+//           <label>Number of guests: </label>
+//           <input
+//             type="number"
+//             value={numberOfGuests}
+//             onChange={(e) => setNumberOfGuests(e.target.value)}
+//           />
+//         </div>
+//         <div className="py-3 px-3 border-t">
+
+//           <label>Your full name: </label>
+//           <input
+//             type="text"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//           />
+//         </div>
+//         <div className="py-3 px-3 border-t">
+
+//           <label>Phone number: </label>
+//           <input
+//             type="tel"
+//             value={phone}
+//             onChange={(e) => {
+//               const newPhone = e.target.value;
+//               if (/^\d*$/.test(newPhone) || newPhone === "") {
+//                 setPhone(newPhone);
+//               }
+//             }}
+//             onBlur={() =>
+//               !isPhoneNumberValid(phone) && phone !== "" && setPhone("")
+//             }
+//           />
+//           {!isPhoneNumberValid(phone) && phone !== "" && (
+//             <p>Please enter a 10-digit phone number</p>
+//           )}
+//         </div>
+
+//         <button
+//           onClick={bookThisPlace}
+//           className={`primary mt-4 mb-4 ${isFormValid() ? '' : 'disabled'}`}
+//           disabled={!isFormValid()}
+//           style={!isFormValid() ? { cursor: 'not-allowed', opacity: 0.6 } : {}}
+//         >
+//           Book for
+//           {numberOfNights > 0 && <span> ${numberOfNights * place.price}</span>}
+//         </button>
+
+
+
+//         {/* <Elements stripe={stripePromise}>
+//           <CheckoutForm />
+//         </Elements> */}
+
+
+
+
+//         {parseInt(numberOfGuests, 10) > maxGuests && (
+//           <div className="text-red-500 mt-2">
+//             Number of guests exceeds the maximum allowed ({maxGuests} guests).
+//           </div>
+//         )}
+//         {bookingSuccess && (
+//           <div className="text-green-500 mt-2">
+//             Success! You are going to travel now!
+//           </div>
+//         )}
+//         {errorMessage && (
+//           <div className="text-red-500 mt-2">
+//             {errorMessage}
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { differenceInCalendarDays, eachDayOfInterval, addDays } from "date-fns";
@@ -5,6 +264,11 @@ import { useNavigate } from "react-router-dom";
 import { UserContext } from "./UserContext.jsx";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
+import StripeCheckout from 'react-stripe-checkout';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+const MySwal = withReactContent(Swal);
 
 export function excludeSingleDate(bookedDates) {
   const missingDates = [];
@@ -20,9 +284,7 @@ export function excludeSingleDate(bookedDates) {
       missingDates.push(...missingDatesInInterval);
     }
   }
-
   return [...missingDates, ...bookedDates];
-
 }
 
 export function removeDuplicates(dates) {
@@ -40,11 +302,12 @@ export default function BookingWidget({ place }) {
   const [numberOfGuests, setNumberOfGuests] = useState('1');
   const [bookedDates, setBookedDates] = useState([]);
   const [maxGuests, setMaxGuests] = useState(1);
-  const [bookingId, setBookingId] = useState(null);
+  const [setBookingId] = useState(null);
   const [bookingSuccess, setBookingSuccess] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
-  //const [redirect, setRedirect] = useState('');
+
+  const stripePublicKey = 'pk_test_51O3kBxGTAB6CtxdhKOcpMUBtDiJc0XCrsWoEOB2BKBqxxszz8XsU0PwykGvtZsezvFeFyd1bXyYDmlscVNqAFh6G00ZedrV42z';
 
   useEffect(() => {
     axios.get(`/bookings/${place._id}`).then(response => {
@@ -78,10 +341,42 @@ export default function BookingWidget({ place }) {
     numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
   }
 
+  const handleSuccess = () => {
+    MySwal.fire({
+      icon: 'success',
+      title: 'Payment was successful',
+    });
+  };
+  const handleFailure = () => {
+    MySwal.fire({
+      icon: 'error',
+      title: 'Payment was not successful',
+    });
+  };
+
+  const payNow = async token => {
+    try {
+      const response = await axios({
+        url: `http://localhost:5173/place/${place._id}/process-payment`,
+        method: 'post',
+        data: {
+          amount: place.price * 100,
+          token,
+        },
+      });
+      if (response.status === 200) {
+        handleSuccess();
+      }
+    } catch (error) {
+      handleFailure();
+      console.log(error);
+    }
+  };
+
   async function bookThisPlace() {
 
     if (!user) {
-      
+
       alert('Please log in to book this place');
       //navigate('/login');
       return;
@@ -123,6 +418,9 @@ export default function BookingWidget({ place }) {
       setErrorMessage("Please register or loging");
     }
   }
+
+
+  
 
   const isPhoneNumberValid = (phoneNumber) => /^\d{10}$/.test(phoneNumber);
 
@@ -212,7 +510,7 @@ export default function BookingWidget({ place }) {
           )}
         </div>
 
-        <button
+        {/* <button
           onClick={bookThisPlace}
           className={`primary mt-4 mb-4 ${isFormValid() ? '' : 'disabled'}`}
           disabled={!isFormValid()}
@@ -220,7 +518,25 @@ export default function BookingWidget({ place }) {
         >
           Book for
           {numberOfNights > 0 && <span> ${numberOfNights * place.price}</span>}
-        </button>
+        </button> */}
+
+
+
+        <StripeCheckout
+          onClick={bookThisPlace}
+          className={`primary mt-4 mb-4 ${isFormValid() ? '' : 'disabled'}`}
+          disabled={!isFormValid()}
+          style={!isFormValid() ? { cursor: 'not-allowed', opacity: 0.6 } : {}}
+          stripeKey={stripePublicKey}
+          label="Book Now"
+          name="Pay With Credit Card"
+          amount={numberOfNights * place.price * 100}
+          description={`Your total is $${numberOfNights * place.price}`}
+          token={payNow}
+
+        />
+
+
 
 
         {parseInt(numberOfGuests, 10) > maxGuests && (
