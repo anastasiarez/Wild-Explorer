5ec55df704f4a8e1a63917fbf96e59ad050a5e82
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-12-02 12:23:31 -0600

    remove console.log and add a catch error in useEffect

--- frontend/src/UserContext.jsx
+++ frontend/src/UserContext.jsx
@@ -7,13 +7,17 @@ const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [ready, setReady] = useState(false);
 
+
   useEffect(() => {
+    
     if (!user) {
       axios.get('/user/profile')
         .then(({ data }) => {
           setUser(data);
           setReady(true);
-        });
+        }).catch((e)=> {
+          console.log("error", e);
+        })
     }
   }, []);
   return (
